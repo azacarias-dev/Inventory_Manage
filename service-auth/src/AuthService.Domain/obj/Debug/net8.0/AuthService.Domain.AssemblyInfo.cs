00fbda06c5b3edfa05fe555d31d25b23b67b6443
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-07-14 10:11:09 -0600

    fix: correccion de dependencias en distintas capas

--- service-auth/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ service-auth/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a1899d2fe01c7616d9957ae8f61497f34077ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f95dc8323bd2d98a689bcab773187658784d8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
