e4d3fe2f80f59a3206875c72a370851be5e0038c
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-07-14 10:56:25 -0600

    docs: archivos de cache

--- service-auth/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ service-auth/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2666b2e0528b07b03e9edc2f1a6e8b9872be3b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e062038d1150fa9cee0f3dcb13a13b2941f9ef2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
